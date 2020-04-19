7fbf674e835b506cae1da7aebd46af77547c7de1
Mourad7 <mourad.djellouli.algerie@viacesi.fr>
2020-04-19 16:58:06 -0400

    Add files via upload

---
+++ shortAdress.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.24;
+
+contract Learning {
+
+    constructor () public payable {
+        // Deploy contract with 1000 wei for testing purpose
+        require(msg.value == 1000);
+    }
+
+    function test() public returns (bool) {
+        // Set minimum gas limit as 700 to send ether to 0x1
+        //same as lostEther but removed a 0 from the address
+        transfer(0x000000000000000000000000000000000000000, 1, 700);
+        return true;
+    }
+
+    function transfer(address _account, uint _wei, uint _gas) private {
+        require(_account.call.value(_wei).gas(_gas)());
+    }
+}
